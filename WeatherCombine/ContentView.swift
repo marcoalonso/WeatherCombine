36bb5710d8891cead85adff4f2cc616059ca6063
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-20 21:22:47 -0600

    Initial Commit

---
+++ WeatherCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherCombine
+//
+//  Created by Marco Alonso on 20/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherCombine/WeatherCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCombineApp.swift
+//  WeatherCombine
+//
+//  Created by Marco Alonso on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
